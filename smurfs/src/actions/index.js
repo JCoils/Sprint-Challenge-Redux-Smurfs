b964926b9e156407615fa60958e3d27098e40d02
JordanG <jmg236@yahoo.com>
2019-05-24 12:54:29 -0500

    actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,3 +1,26 @@
+import axios from "axios";
+export const FETCHED = "FETCHED";
+export const FETCHING = "FETCHING";
+export const POSTING = "POSTING";
+export const POSTED = "POSTED";
+
+export const fetchSmurfs = () => {
+  const request = axios.get(`http://localhost:3333/smurfs`);
+   return dispatch => {
+
+    dispatch({ type: FETCHING });
+    request
+    .then(res => {
+      dispatch({ type: FETCHED, payload: res.data });
+    })
+    .catch(err => {
+      console.log(err);
+      dispatch({ type: ERROR });
+    });
+};
+};
+
+
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
